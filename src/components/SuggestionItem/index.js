eef2ac6db097d551d8955c859dc0348e061644d9
yoshithajammu <yoshithajammu23@gmail.com>
2024-07-12 12:53:45 +0000

    commit messge

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,30 @@
+// Write your code here
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionDetails, updateSearchInput} = props
+  const {suggestion} = suggestionDetails
+
+  const onClickSuggestion = () => {
+    updateSearchInput(suggestion)
+  }
+
+  return (
+    <li className="suggestion-item">
+      <p className="suggestion-text">{suggestion}</p>
+      <button
+        type="button"
+        className="arrow-button"
+        onClick={onClickSuggestion}
+      >
+        <img
+          alt="arrow"
+          className="arrow"
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default SuggestionItem
